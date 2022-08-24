2cf3f9eb06d7ac126130ab5f4cc5d1a62b3fb962
nehemias soto <nehemiassoto.epet20@gmail.com>
2022-08-24 15:34:01 -0300

    Creando animacion inicio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p class="Titulo-carga">
+        <p class="Titulo-carga App-logo-titulo">
           ¡Booky!
         </p>
         
