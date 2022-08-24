88b26e5a23eab466757bd63488ed685e1608627d
nehemias soto <nehemiassoto.epet20@gmail.com>
2022-08-24 15:27:50 -0300

    Agregando detalles de inicio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo epet20.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p class="Titulo-carga">
           Hola
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
